67414886dda4e6a9adbda6adb54fbb825642deae
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-05-16 14:43:03 -0400

    Update main_blueprint.py

--- src/routes/main_blueprint.py
+++ src/routes/main_blueprint.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from controller.main_controller import home, help, findRoute
+from controller.main_controller import home, help, getroute
 
 main_bp = Blueprint('main_blueprint', __name__)
 main_bp.route('/', methods=['GET'])(help)
 main_bp.route('/help', methods=['GET'])(home)
-main_bp.route('/findroute', methods=['POST'])(findRoute)
+main_bp.route('/getroute', methods=['POST'])(getroute)
