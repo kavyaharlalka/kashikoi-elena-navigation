f0e4de6e2c9b3bba8f985f2e322403e96ec13ff0
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-05-16 15:19:07 -0400

    Controller algorithm dijkstra

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, request
+import controller.route_manager as route_manager
+import api.google_maps_client as gmap_client
+import osmnx as ox
 
 def home():
     return render_template("index.html")
@@ -6,5 +9,11 @@ def home():
 def help():
     return render_template("help.html")
 
-def findRoute():
-    pass
+def getroute():
+    data = request.get_json(force=True)
+    graph = route_manager.create_graph("University of Massachusetts Amherst", dist=700)
+    graph = route_manager.populate_graph(graph)
+    graph = route_manager.modify_graph_elevate(graph)
+    source = ox.nearest_nodes(graph, gmap_client.get_coordinates(data['source']))
+    destination = ox.nearest_nodes(graph, gmap_client.get_coordinates(data['destination']))
+    return route_manager.get_shortest_path(graph, source, destination, float(data['path_percentage']), bool(data['minimize_elevation_gain']))
---
+++ src/controller/route_manager.py
@@ -0,0 +1,57 @@
+import networkx
+import osmnx as ox
+from config import GMAP_API_KEY
+
+COORDINATE_X = 'x'
+COORDINATE_Y = 'y'
+
+def get_coordinates_from_nodes(graph, nodes_to_convert):
+    return [(graph.nodes[node][COORDINATE_Y], graph.nodes[node][COORDINATE_X]) for node in nodes_to_convert]
+
+
+def get_shortest_path(graph, start, end, path_percentage, minimize_elevation_gain):
+    print(start)
+    print(end)
+
+    nx_graph = networkx.Graph(graph)
+    shortest_path_distance = networkx.dijkstra_path_length(nx_graph, source=start, target=end, weight='length')
+    max_distance = path_percentage * shortest_path_distance
+    def custom_weight_func(u, v, data):
+        distance = data['length']
+        elevation_gain = abs(nx_graph.nodes[v]['elevation'] - nx_graph.nodes[u]['elevation'])
+
+        if distance > max_distance or elevation_gain < 0:
+            return None  # Ignore paths that exceed the max_distance
+
+        # You can customize how elevation gain is prioritized (minimized or maximized)
+        return 1.0/elevation_gain if minimize_elevation_gain != 0 else 0
+
+    short_path = networkx.dijkstra_path(nx_graph, source=start, target=end, weight=custom_weight_func)
+    coord_path = get_coordinates_from_nodes(graph, short_path)
+    return {"nodes": short_path, "coordinates": coord_path}
+
+
+def create_graph(loc, dist, transport_mode='walk', loc_type='address'):
+    if loc_type == "address":
+        return ox.graph_from_address(loc, dist=dist, network_type=transport_mode)
+    elif loc_type == "points":
+        return ox.graph_from_point(loc, dist=dist, network_type=transport_mode)
+    return None
+
+
+def populate_graph(graph):
+    graph = ox.add_node_elevations_google(graph, api_key=GMAP_API_KEY)
+    graph = ox.add_edge_grades(graph)
+    return graph
+
+
+def cost_function(length, gradient):
+    penalty_term = gradient ** 2
+    return (length *penalty_term)**2
+
+# add cost function to graph
+def modify_graph_elevate(graph):
+    for _,__,___, data in graph.edges(keys=True, data=True):
+        data['impedance'] = -cost_function(data['length'], data['grade'])
+        data['rise'] = data['length'] * data['grade']
+    return graph
