3e9348132c8014d38c8594f55efd91b6e02295f4
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-05-19 21:58:44 -0400

    Get latitude and longitude for source and destination

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -10,10 +10,15 @@ def help():
     return render_template("help.html")
 
 def getroute():
+    # try:
     data = request.get_json(force=True)
     graph = route_manager.create_graph("University of Massachusetts Amherst", dist=700)
     graph = route_manager.populate_graph(graph)
     graph = route_manager.modify_graph_elevate(graph)
-    source = ox.nearest_nodes(graph, gmap_client.get_coordinates(data['source']))
-    destination = ox.nearest_nodes(graph, gmap_client.get_coordinates(data['destination']))
+    source_coordinates = gmap_client.get_coordinates(data['source'])
+    source = ox.nearest_nodes(graph, source_coordinates['lat'], source_coordinates['lng'])
+    destination_coordinates = gmap_client.get_coordinates(data['destination'])
+    destination = ox.nearest_nodes(graph, destination_coordinates['lat'], destination_coordinates['lng'])
     return route_manager.get_shortest_path(graph, source, destination, float(data['path_percentage']), bool(data['minimize_elevation_gain']))
+    # except Exception as e:
+    #    print(e)
