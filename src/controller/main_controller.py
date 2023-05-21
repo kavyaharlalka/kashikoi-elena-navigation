ae61c3df6ebe694ab60ba81b88e396102605e58d
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-05-21 03:32:14 -0400

    Fixed return type of algorithm shortest path

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -44,15 +44,30 @@ def get_route():
         raise BadRequest(description="Given transportation mode is not supported")
 
     try:
-        graph = route_manager.create_graph("University of Massachusetts Amherst", 700, data[constants.REQUEST_JSON_TRANSPORTATION_MODE_KEY])
-        graph = route_manager.populate_graph(graph)
-        graph = route_manager.modify_graph_elevate(graph)
         source_coordinates = gmap_client.get_coordinates(data[constants.REQUEST_JSON_SOURCE_KEY])
-        source = ox.nearest_nodes(graph, source_coordinates[constants.COORDINATES_LATITUDE], source_coordinates[constants.COORDINATES_LONGITUDE])
         destination_coordinates = gmap_client.get_coordinates(data[constants.REQUEST_JSON_DESTINATION_KEY])
-        destination = ox.nearest_nodes(graph, destination_coordinates[constants.COORDINATES_LATITUDE], destination_coordinates[constants.COORDINATES_LONGITUDE])
-        return route_manager.get_shortest_path(data[constants.REQUEST_JSON_ALGORITHM_ID_KEY], graph, source,
+        graph = route_manager.getGraphOject((source_coordinates[constants.COORDINATES_LATITUDE], source_coordinates[constants.COORDINATES_LONGITUDE]),
+                                            (destination_coordinates[constants.COORDINATES_LATITUDE], destination_coordinates[constants.COORDINATES_LONGITUDE]),
+                                            data[constants.REQUEST_JSON_TRANSPORTATION_MODE_KEY])
+        # graph = route_manager.populate_graph(graph)
+        # graph = route_manager.modify_graph_elevate(graph)
+        source = ox.nearest_nodes(graph, source_coordinates[constants.COORDINATES_LONGITUDE], source_coordinates[constants.COORDINATES_LATITUDE])
+        destination = ox.nearest_nodes(graph, destination_coordinates[constants.COORDINATES_LONGITUDE], destination_coordinates[constants.COORDINATES_LATITUDE])
+
+        best_path_algorithm_result = route_manager.get_shortest_path(data[constants.REQUEST_JSON_ALGORITHM_ID_KEY], graph, source,
                                                destination, data[constants.REQUEST_JSON_PATH_PERCENTAGE_KEY],
                                                data[constants.REQUEST_JSON_MINIMIZE_ELEVATION_GAIN_KEY])
+        best_path_algorithm_stats = [best_path_algorithm_result['coordinates'],
+                                     sum(ox.utils_graph.get_route_edge_attributes(graph, best_path_algorithm_result['nodes'], 'length')),
+                                     route_manager.getElevation(graph, best_path_algorithm_result['nodes'], "gain"),
+                                     route_manager.getElevation(graph, best_path_algorithm_result['nodes'], "drop")]
+
+        shortest_path_nodes = ox.distance.shortest_path(graph, orig=source, dest=destination)
+        shortest_path_distance = sum(ox.utils_graph.get_route_edge_attributes(graph, shortest_path_nodes, 'length'))
+        shortestPathStats = [route_manager.get_coordinates_from_nodes(graph, shortest_path_nodes),
+                             shortest_path_distance,
+                             route_manager.getElevation(graph, shortest_path_nodes, "gain"),
+                             route_manager.getElevation(graph, shortest_path_nodes, "drop")]
+        return { "shortest_path": shortestPathStats, "best_path": best_path_algorithm_stats }
     except Exception as e:
         abort(e.code, str(e))
--- src/controller/route_manager.py
+++ src/controller/route_manager.py
@@ -1,7 +1,11 @@
 import networkx
+import numpy as np
 import osmnx as ox
 from config import GMAP_API_KEY
 from enum import Enum
+import pickle as p
+import os
+
 
 COORDINATE_X = 'x'
 COORDINATE_Y = 'y'
@@ -83,11 +87,90 @@ def create_graph(location, distance, transportation_mode):
 
     return ox.graph_from_address(location, dist=distance, network_type=TRANSPORTATION_MODE[transportation_mode])
 
+def getGraphOject(start_location, end_location, transportation_mode):
+    assert start_location is not None, "Invalid Location"
+    assert end_location is not None, "Invalid Distance"
+    assert transportation_mode in TRANSPORTATION_MODE, "Invalid Transportation Mode"
+
+    if os.path.exists("./graph.p"):
+        G = p.load(open("graph.p", "rb"))
+        print("Found Existing Graph")
+    else:
+        print("Did not find existing Graph. Downloading !!!!")
+        G = ox.graph_from_point(start_location, dist=30000, dist_type="network", network_type=TRANSPORTATION_MODE[transportation_mode])
+        G = ox.elevation.add_node_elevations_google(G, api_key=GMAP_API_KEY)
+        p.dump(G, open("graph.p", "wb"))
+        print("Saved Graph !!!!")
+
+    end_node = ox.distance.nearest_nodes(G, end_location[1], end_location[0], return_dist=False)
+    end_location = G.nodes[end_node]
+    lat1 = end_location['y']
+    lon1 = end_location['x']
+    for node, data in G.nodes(data=True):
+        lat2 = G.nodes[node]['y']
+        lon2 = G.nodes[node]['x']
+        data['distFromDest'] = calculate_spherical_distance(lat1, lon1, lat2, lon2)
+    return G
+
+def calculate_spherical_distance(lat1, lon1, lat2, lon2, r=6371008.8):
+    # Convert degrees to radians
+    coordinates = lat1, lon1, lat2, lon2
+    # radians(c) is same as c*pi/180
+    phi1, lambda1, phi2, lambda2 = [
+        np.radians(c) for c in coordinates
+    ]
+    # Apply the haversine formula
+    a = (np.square(np.sin((phi2 - phi1) / 2)) + np.cos(phi1) * np.cos(phi2) *
+         np.square(np.sin((lambda2 - lambda1) / 2)))
+    d = 2 * r * np.arcsin(np.sqrt(a))
+    return d
+
+def getElevation(G, route, mode, pairFlag = False):
+    """ Computes the cost of a route which is the elevation of the route.
+    Parameters:
+        route -> List of Node IDs
+        mode -> mode of elevation
+        pairFlag -> boolean to indicate if individual cost between the nodes needs to be returned
+    Returns:
+        total -> total cost of the route
+        pairElevList -> list of individual costs of the route [Optional]
+    """
+    total = 0
+    if pairFlag : pairElevList = []
+    for i in range(len(route)-1):
+        if mode == "gain":
+            diff = getCost(G, route[i],route[i+1],"gain")
+        elif mode == "vanilla":
+            diff = getCost(G, route[i],route[i+1],"vanilla")
+        else:
+            diff = getCost(G, route[i],route[i+1],"drop")
+        total += diff
+        if pairFlag : pairElevList.append(diff)
+    if pairFlag:
+        return total, pairElevList
+    else:
+        return total
+
+
+def getCost(G, n1, n2, mode="vanilla"):
+    """ defines the cost between two nodes """
+
+    if n1 is None or n2 is None: return
+    if mode == "gain":
+        return max(0.0, G.nodes[n2]["elevation"] - G.nodes[n1]["elevation"])
+    elif mode == "drop":
+        if G.nodes[n2]["elevation"] - G.nodes[n1]["elevation"] > 0:
+            return 0.0
+        else:
+            return G.nodes[n1]["elevation"] - G.nodes[n2]["elevation"]
+    else:
+        return abs(G.nodes[n1]["elevation"] - G.nodes[n2]["elevation"])
+
 
 def populate_graph(graph):
     assert graph is not None, "Invalid Location"
 
-    graph = ox.add_node_elevations_google(graph, api_key=GMAP_API_KEY)
+    # graph = ox.add_node_elevations_google(graph, api_key=GMAP_API_KEY)
     graph = ox.add_edge_grades(graph)
     return graph
 
