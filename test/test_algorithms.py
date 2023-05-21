275eedacd1d9eda8062c7010d79b23959dc9d8ae
r-acharya <77462752+r-acharya@users.noreply.github.com>
2023-05-21 03:06:18 -0400

    Fixed test cases and deleted old folder

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -1,20 +1,21 @@
 import pytest
-import controller.route_manager as route_manager
+import src.controller.route_manager as route_manager
 import pickle as p
 import networkx as nx
 import osmnx as ox
 
-# @pytest.fixture
-# def input_for_test():
-#     """Create a dummy graph with origin and destination locations for testing"""
-#     graph = create_graph("University of Massachusetts Amherst", dist=700)
-#     origin_node = ox.nearest_nodes(graph, 42.3878210, -72.5239110)
-#     destination_node = 5850477768
-
 @pytest.fixture
+def input_for_test():
+    """Create a dummy graph with origin and destination locations for testing
+    
+    """
+    graph = create_graph("University of Massachusetts Amherst", dist=700)
+    origin_node = ox.nearest_nodes(graph, 42.3878210, -72.5239110)
+    destination_node = 5850477768
+
 def test_cost_function():
     """Test function for the cost function. The assertion should pass if the cost function works correctly.
 
     """
     length, gradient = 10, 2
-    assert route_manager.cost_function(length, gradient) == 1600
+    assert route_manager.cost_function(length, gradient) == 1600, "incorrect cost"
