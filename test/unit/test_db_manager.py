3af6f32eacaa0c16f0eab4141522d2e18dd73b70
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-05-22 13:51:57 -0400

    Unit test for valid db operations

--- test/unit/test_db_manager.py
+++ test/unit/test_db_manager.py
@@ -30,3 +30,33 @@ def test_insert_into_database_invalid_data():
         db_manager.insert_into_database(source, destination, algorithm_id, path_percent, minimize_elevation_gain, transportation_mode, json_result)
 
 
+def test_insert_into_database():
+    expected_output_number_of_records = 1
+
+    source = 'Dummy source'
+    destination = 'Dummy destination'
+    algorithm_id = 0
+    path_percent = 200
+    minimize_elevation_gain = True
+    transportation_mode = 1
+    json_result = {'testingKeyStr': 'testingValue', 'testingKeyInt': 10, 'testingKeyBool': True}
+
+    # In case there are old testing records in the database
+    cleanup_delete_record(source, destination, algorithm_id, path_percent, minimize_elevation_gain, transportation_mode)
+
+    db_manager.insert_into_database(source, destination, algorithm_id, path_percent, minimize_elevation_gain, transportation_mode, json_result)
+
+    connection = sqlite3.connect(config.DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute(
+        f'select result from {config.TABLE_NAME} where source = ? and destination = ? and algorithm_id = ? and path_percent = ? and minimize_elevation_gain = ? and transportation_mode = ?;',
+        (source, destination, algorithm_id, path_percent, minimize_elevation_gain, transportation_mode))
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    assert len(result) == expected_output_number_of_records
+    assert json.loads(result[0][0]) == json_result
+
+    # Cleanup added record
+    cleanup_delete_record(source, destination, algorithm_id, path_percent, minimize_elevation_gain, transportation_mode)
