9917a5201c25148c669d097b01c4709e32c9f1d8
r-acharya <77462752+r-acharya@users.noreply.github.com>
2023-05-22 04:45:03 -0400

    Added more integration tests

--- test/api_test.py
+++ test/api_test.py
@@ -1,23 +1,45 @@
 import requests
-import time
 
 HOST = "127.0.0.1"
 PORT = "5000"
 server_url = "http://" + HOST + ":" + PORT
 
-# request_json = {
-#                  "source": "1039 North Pleasant Street, Amherst, MA, USA",
-#                  "destination": "12 Brandywine, Amherst, MA, USA",
-#                  "algorithm_id": 0,
-#                  "path_percentage": 100,
-#                  "minimize_elevation_gain": True,
-#                  "transportation_mode": 1
-#                  }
-# post_time_start = time.time()
-# server_response = requests.post(f'{server_url}/getroute', json= request_json)
-# post_time_end = time.time()
-# server_json = server_response.json()
-# # print(server_json)
+
+def test_valid_input():
+    request_json = {
+                    "source": "1039 North Pleasant Street, Amherst, MA, USA",
+                    "destination": "12 Brandywine, Amherst, MA, USA",
+                    "algorithm_id": 0,
+                    "path_percentage": 100,
+                    "minimize_elevation_gain": True,
+                    "transportation_mode": 1
+                    }
+    response = requests.post(f'{server_url}/getroute', json= request_json)
+    assert response.status_code == 200
+
+def test_illegal_source():
+    request_json = {
+                    "source": "Delhi, India",
+                    "destination": "12 Brandywine, Amherst, MA, USA",
+                    "algorithm_id": 0,
+                    "path_percentage": 100,
+                    "minimize_elevation_gain": True,
+                    "transportation_mode": 1
+                    }
+    response = requests.post(f'{server_url}/getroute', json= request_json)
+    assert response.status_code == 400
+
+def test_illegal_destination():
+    request_json = {
+                    "source": "1039 North Pleasant Street, Amherst, MA, USA",
+                    "destination": "Delhi, India",
+                    "algorithm_id": 0,
+                    "path_percentage": 100,
+                    "minimize_elevation_gain": True,
+                    "transportation_mode": 1
+                    }
+    response = requests.post(f'{server_url}/getroute', json= request_json)
+    assert response.status_code == 400
 
 def test_empty_source():
     request_json = {
@@ -51,6 +73,30 @@ def test_illegal_algorithm_id():
                     "path_percentage": 100,
                     "minimize_elevation_gain": True,
                     "transportation_mode": 1
+                    }
+    response = requests.post(f'{server_url}/getroute', json= request_json)
+    assert response.status_code == 400
+
+def test_illegal_path_percentage():
+    request_json = {
+                    "source": "1039 North Pleasant Street, Amherst, MA, USA",
+                    "destination": "12 Brandywine, Amherst, MA, USA",
+                    "algorithm_id": 0,
+                    "path_percentage": 700,
+                    "minimize_elevation_gain": True,
+                    "transportation_mode": 1
+                    }
+    response = requests.post(f'{server_url}/getroute', json= request_json)
+    assert response.status_code == 400
+
+def test_illegal_transportation_mode():
+    request_json = {
+                    "source": "1039 North Pleasant Street, Amherst, MA, USA",
+                    "destination": "12 Brandywine, Amherst, MA, USA",
+                    "algorithm_id": 0,
+                    "path_percentage": 100,
+                    "minimize_elevation_gain": True,
+                    "transportation_mode": 4
                     }
     response = requests.post(f'{server_url}/getroute', json= request_json)
     assert response.status_code == 400
